58d2dd7b890073cdaa9abca8d68495cc8a5fb7ff
philbowman <47841859+philbowman@users.noreply.github.com>
2021-10-19 14:04:11 +0300

    function names

--- quotient/__init__.py
+++ quotient/__init__.py
@@ -15,31 +15,31 @@ def compiles():
 
 
 @check50.check(compiles)
-def test041():
+def test83():
     """input of 8 and 3 yields output of 2, 2, and 2.667"""
     out = check50.run("./quotient").stdin("8").stdin("3").stdout()
     compare_outputs(8, 3, out)
 
 @check50.check(compiles)
-def test041():
+def test93():
     """input of 9 and 3 yields output of 3, 0, and 3.000"""
     out = check50.run("./quotient").stdin("9").stdin("3").stdout()
     compare_outputs(8, 3, out)
 
 @check50.check(compiles)
-def test041():
+def test324():
     """input of 99 and 25 yields output of 3, 24, and 3.960"""
     out = check50.run("./quotient").stdin("99").stdin("25").stdout()
     compare_outputs(8, 3, out)
 
 @check50.check(compiles)
-def test041():
+def test1999():
     """input of 1 and 999 yields output of 0, 1, and 0.001"""
     out = check50.run("./quotient").stdin("1").stdin("999").stdout()
     compare_outputs(1, 999, out)
 
 @check50.check(compiles)
-def test041():
+def testrand1():
     """input of random numbers yields the correct output (1)"""
     dividend = randint(1, 999)
     divisor = randint(1, dividend)
@@ -48,7 +48,7 @@ def test041():
     compare_outputs(dividend, divisor, out)
 
 @check50.check(compiles)
-def test041():
+def testrand2():
     """input of random numbers yields the correct output (2)"""
     divisor = randint(1, 999)
     dividend = randint(1, divisor)
@@ -57,7 +57,7 @@ def test041():
     compare_outputs(dividend, divisor, out)
 
 @check50.check(compiles)
-def test041():
+def testrand3():
     """input of random numbers yields the correct output (3)"""
     divisor = randint(1, 999)
     dividend = randint(1, 999)
