cb959106290134710f3007bf6619b55a0a57383c
philbowman <47841859+philbowman@users.noreply.github.com>
2021-10-21 12:01:29 +0300

    debug

--- readability/__init__.py
+++ readability/__init__.py
@@ -41,14 +41,13 @@ def multiple_sentences():
 @check50.check()
 def multiple_sentences_complex():
     """handles multiple more complex sentences"""
-    from re import match
 
     expected = "College Student"
     cheated = "Grade 10"
     actual = check50.run("./readability").stdin("It was a bright cold day in April, and the clocks were striking thirteen. Winston Smith, his chin nuzzled into his breast in an effort to escape the vile wind, slipped quickly through the glass doors of Victory Mansions, though not quickly enough to prevent a swirl of gritty dust from entering along with him.").stdout()
-    if not match(expected, actual):
+    if expected != actual:
         help = None
-        if match(cheated, actual):
+        if actual = cheated:
             help = "It looks like you may have used a solution to a previous problem set. This is against this course's policy on academic honesty. Please have a conversation with your teacher about this, delete the code you've written so far, and start over."
         raise check50.Mismatch(expected, actual, help=help)
 """
