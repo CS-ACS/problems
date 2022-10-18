4687bf1dad2bff78d32da85d952d640a3bd996ee
philbowman <47841859+philbowman@users.noreply.github.com>
2022-10-18 10:07:24 +0300

    cash to input whole numbers, not decimals

--- cash/__init__.py
+++ cash/__init__.py
@@ -17,31 +17,31 @@ def compiles():
 @check50.check(compiles)
 def test041():
     """input of 0.41 yields output of 4"""
-    check50.run("./cash").stdin("0.41").stdout(coins(4), "4\n").exit(0)
+    check50.run("./cash").stdin("41").stdout(coins(4), "4\n").exit(0)
 
 
 @check50.check(compiles)
 def test001():
     """input of 0.01 yields output of 1"""
-    check50.run("./cash").stdin("0.01").stdout(coins(1), "1\n").exit(0)
+    check50.run("./cash").stdin("1").stdout(coins(1), "1\n").exit(0)
 
 
 @check50.check(compiles)
 def test015():
     """input of 0.15 yields output of 2"""
-    check50.run("./cash").stdin("0.15").stdout(coins(2), "2\n").exit(0)
+    check50.run("./cash").stdin("15").stdout(coins(2), "2\n").exit(0)
 
 
 @check50.check(compiles)
 def test160():
     """input of 1.6 yields output of 7"""
-    check50.run("./cash").stdin("1.6").stdout(coins(7), "7\n").exit(0)
+    check50.run("./cash").stdin("160").stdout(coins(7), "7\n").exit(0)
 
 
 @check50.check(compiles)
 def test230():
     """input of 23 yields output of 92"""
-    check50.run("./cash").stdin("23").stdout(coins(92), "92\n").exit(0)
+    check50.run("./cash").stdin("2300").stdout(coins(92), "92\n").exit(0)
 
 
 @check50.check(compiles)
@@ -49,7 +49,7 @@ def test420():
     """input of 4.2 yields output of 18"""
     from re import search
     expected = "18\n"
-    actual = check50.run("./cash").stdin("4.2").stdout()
+    actual = check50.run("./cash").stdin("420").stdout()
     if not search(coins(18), actual):
         help = None
         if search(coins(22), actual):
